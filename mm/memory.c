6853638238e28d64e4ce8d5abf12d0561ded835f
Peter Jung <admin@ptr1337.dev>
2022-12-31 18:33:10 +0100

    Revert "mm: add vma_has_locality()"
    This reverts commit aced6e6c7b4d1946328ad13d041cb984e7692561.

--- mm/memory.c
+++ mm/memory.c
@@ -1458,7 +1458,8 @@ again:
 					force_flush = 1;
 					set_page_dirty(page);
 				}
-				if (pte_young(ptent) && likely(vma_has_locality(vma)))
+				if (pte_young(ptent) &&
+				    likely(!(vma->vm_flags & VM_SEQ_READ)))
 					mark_page_accessed(page);
 			}
 			rss[mm_counter(page)]--;
@@ -5170,8 +5171,8 @@ static inline void mm_account_fault(struct pt_regs *regs,
 #ifdef CONFIG_LRU_GEN
 static void lru_gen_enter_fault(struct vm_area_struct *vma)
 {
-	/* the LRU algorithm only applies to accesses with locality */
-	current->in_lru_fault = vma_has_locality(vma);
+	/* the LRU algorithm doesn't apply to sequential or random reads */
+	current->in_lru_fault = !(vma->vm_flags & (VM_SEQ_READ | VM_RAND_READ));
 }
 
 static void lru_gen_exit_fault(void)
