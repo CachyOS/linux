50654d485ba303c1f1f68cd218e3b6bb658014e9
Peter Zijlstra <peterz@infradead.org>
2023-03-25 00:14:04 +0100

    sched/eevdf: Better handle mixed slice length
    In the case where there are different request sizes in the tree, the smaller
    requests tend to be dominated by the larger. Also note how the EEVDF lag limits
    are based on r_max.
    
    Therefore; add a heuristic that for the mixed request size case, moves
    smaller requests to placement strategy #2 which ensures they're
    immidiately eligible and and due to their smaller (virtual) deadline
    will cause preemption.
    
    NOTE: this relies on update_entity_lag() to impose lag limits above
    a single slice.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -5,6 +5,7 @@
  * sleep+wake cycles. EEVDF placement strategy #1, #2 if disabled.
  */
 SCHED_FEAT(PLACE_LAG, true)
+SCHED_FEAT(PLACE_FUDGE, true)
 SCHED_FEAT(PLACE_DEADLINE_INITIAL, true)
 SCHED_FEAT(RUN_TO_PARITY, true)
 
