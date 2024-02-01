9d88fde9b962195e0fa6a81b19370e658fd1fbda
Piotr Gorski <lucjan.lucjanov@gmail.com>
2024-02-01 14:19:11 +0100

    Drop previous patches
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -67,7 +67,6 @@ static struct cpufreq_driver amd_pstate_epp_driver;
 static int cppc_state = AMD_PSTATE_UNDEFINED;
 static bool cppc_enabled;
 static bool amd_pstate_prefcore = true;
-static struct quirk_entry *quirks;
 
 /*
  * AMD Energy Preference Performance (EPP)
@@ -111,32 +110,6 @@ static unsigned int epp_values[] = {
  };
 
 typedef int (*cppc_mode_transition_fn)(int);
-
-static struct quirk_entry quirk_amd_7k62 = {
-	.nominal_freq = 2600,
-	.lowest_freq = 550,
-};
-
-static int __init dmi_matched(const struct dmi_system_id *dmi)
-{
-	quirks = dmi->driver_data;
-
-	return 1;
-}
-
-static const struct dmi_system_id amd_pstate_quirks_table[] __initconst = {
-	{
-		.callback = dmi_matched,
-		.ident = "AMD EPYC 7K62",
-		.matches = {
-			DMI_MATCH(DMI_PRODUCT_VERSION, "C1"),
-			DMI_MATCH(DMI_PRODUCT_SERIAL, "FX19911000028"),
-		},
-		.driver_data = &quirk_amd_7k62,
-	},
-	{}
-};
-MODULE_DEVICE_TABLE(dmi, amd_pstate_quirks_table);
 
 static inline int get_mode_idx_from_str(const char *str, size_t size)
 {
@@ -627,19 +600,13 @@ static void amd_pstate_adjust_perf(unsigned int cpu,
 static int amd_get_min_freq(struct amd_cpudata *cpudata)
 {
 	struct cppc_perf_caps cppc_perf;
-	u32 lowest_freq;
 
 	int ret = cppc_get_perf_caps(cpudata->cpu, &cppc_perf);
 	if (ret)
 		return ret;
 
-	if (quirks && quirks->lowest_freq)
-		lowest_freq = quirks->lowest_freq;
-	else
-		lowest_freq = cppc_perf.lowest_freq;
-
 	/* Switch to khz */
-	return lowest_freq * 1000;
+	return cppc_perf.lowest_freq * 1000;
 }
 
 static int amd_get_max_freq(struct amd_cpudata *cpudata)
@@ -652,7 +619,7 @@ static int amd_get_max_freq(struct amd_cpudata *cpudata)
 	if (ret)
 		return ret;
 
-	nominal_freq = READ_ONCE(cpudata->nominal_freq);
+	nominal_freq = cppc_perf.nominal_freq;
 	nominal_perf = READ_ONCE(cpudata->nominal_perf);
 	max_perf = READ_ONCE(cpudata->highest_perf);
 
@@ -667,14 +634,14 @@ static int amd_get_max_freq(struct amd_cpudata *cpudata)
 
 static int amd_get_nominal_freq(struct amd_cpudata *cpudata)
 {
-	u32 nominal_freq;
+	struct cppc_perf_caps cppc_perf;
 
-	if (quirks && quirks->nominal_freq)
-		nominal_freq = quirks->nominal_freq;
-	else
-		nominal_freq = READ_ONCE(cpudata->nominal_freq);
+	int ret = cppc_get_perf_caps(cpudata->cpu, &cppc_perf);
+	if (ret)
+		return ret;
 
-	return nominal_freq;
+	/* Switch to khz */
+	return cppc_perf.nominal_freq * 1000;
 }
 
 static int amd_get_lowest_nonlinear_freq(struct amd_cpudata *cpudata)
@@ -688,7 +655,7 @@ static int amd_get_lowest_nonlinear_freq(struct amd_cpudata *cpudata)
 	if (ret)
 		return ret;
 
-	nominal_freq = READ_ONCE(cpudata->nominal_freq);
+	nominal_freq = cppc_perf.nominal_freq;
 	nominal_perf = READ_ONCE(cpudata->nominal_perf);
 
 	lowest_nonlinear_perf = cppc_perf.lowest_nonlinear_perf;
@@ -854,36 +821,6 @@ free_cpufreq_put:
 	mutex_unlock(&amd_pstate_driver_lock);
 }
 
-/**
- * Get pstate transition delay time from ACPI tables that firmware set
- * instead of using hardcode value directly.
- */
-static u32 amd_pstate_get_transition_delay_us(unsigned int cpu)
-{
-	u32 transition_delay_ns;
-
-	transition_delay_ns= cppc_get_transition_latency(cpu);
-	if (transition_delay_ns == CPUFREQ_ETERNAL)
-		return AMD_PSTATE_TRANSITION_DELAY;
-
-	return transition_delay_ns / NSEC_PER_USEC;
-}
-
-/**
- * Get pstate transition latency value from ACPI tables that firmware set
- * instead of using hardcode value directly.
- */
-static u32 amd_pstate_get_transition_latency(unsigned int cpu)
-{
-	u32 transition_latency;
-
-	transition_latency = cppc_get_transition_latency(cpu);
-	if (transition_latency  == CPUFREQ_ETERNAL)
-		return AMD_PSTATE_TRANSITION_LATENCY;
-
-	return transition_latency;
-}
-
 static int amd_pstate_cpu_init(struct cpufreq_policy *policy)
 {
 	int min_freq, max_freq, nominal_freq, lowest_nonlinear_freq, ret;
@@ -912,21 +849,19 @@ static int amd_pstate_cpu_init(struct cpufreq_policy *policy)
 		goto free_cpudata1;
 
 	min_freq = amd_get_min_freq(cpudata);
-	nominal_freq = amd_get_nominal_freq(cpudata);
-	cpudata->nominal_freq = nominal_freq;
 	max_freq = amd_get_max_freq(cpudata);
+	nominal_freq = amd_get_nominal_freq(cpudata);
 	lowest_nonlinear_freq = amd_get_lowest_nonlinear_freq(cpudata);
 
-	if (min_freq < 0 || max_freq < 0 || min_freq > max_freq || nominal_freq == 0) {
-		dev_err(dev, "min_freq(%d) or max_freq(%d) or nominal_freq(%d)	\
-				value is incorrect\n",	\
-				min_freq, max_freq, nominal_freq);
+	if (min_freq < 0 || max_freq < 0 || min_freq > max_freq) {
+		dev_err(dev, "min_freq(%d) or max_freq(%d) value is incorrect\n",
+			min_freq, max_freq);
 		ret = -EINVAL;
 		goto free_cpudata1;
 	}
 
-	policy->cpuinfo.transition_latency = amd_pstate_get_transition_latency(policy->cpu);
-	policy->transition_delay_us = amd_pstate_get_transition_delay_us(policy->cpu);
+	policy->cpuinfo.transition_latency = AMD_PSTATE_TRANSITION_LATENCY;
+	policy->transition_delay_us = AMD_PSTATE_TRANSITION_DELAY;
 
 	policy->min = min_freq;
 	policy->max = max_freq;
@@ -959,6 +894,7 @@ static int amd_pstate_cpu_init(struct cpufreq_policy *policy)
 	cpudata->min_freq = min_freq;
 	cpudata->max_limit_freq = max_freq;
 	cpudata->min_limit_freq = min_freq;
+	cpudata->nominal_freq = nominal_freq;
 	cpudata->lowest_nonlinear_freq = lowest_nonlinear_freq;
 
 	policy->driver_data = cpudata;
@@ -1375,14 +1311,12 @@ static int amd_pstate_epp_cpu_init(struct cpufreq_policy *policy)
 		goto free_cpudata1;
 
 	min_freq = amd_get_min_freq(cpudata);
-	nominal_freq = amd_get_nominal_freq(cpudata);
-	cpudata->nominal_freq = nominal_freq;
 	max_freq = amd_get_max_freq(cpudata);
+	nominal_freq = amd_get_nominal_freq(cpudata);
 	lowest_nonlinear_freq = amd_get_lowest_nonlinear_freq(cpudata);
-	if (min_freq < 0 || max_freq < 0 || min_freq > max_freq || nominal_freq == 0) {
-		dev_err(dev, "min_freq(%d) or max_freq(%d) or nominal_freq(%d)	\
-				value is incorrect\n",	\
-				min_freq, max_freq, nominal_freq);
+	if (min_freq < 0 || max_freq < 0 || min_freq > max_freq) {
+		dev_err(dev, "min_freq(%d) or max_freq(%d) value is incorrect\n",
+				min_freq, max_freq);
 		ret = -EINVAL;
 		goto free_cpudata1;
 	}
@@ -1395,6 +1329,7 @@ static int amd_pstate_epp_cpu_init(struct cpufreq_policy *policy)
 	/* Initial processor data capability frequencies */
 	cpudata->max_freq = max_freq;
 	cpudata->min_freq = min_freq;
+	cpudata->nominal_freq = nominal_freq;
 	cpudata->lowest_nonlinear_freq = lowest_nonlinear_freq;
 
 	policy->driver_data = cpudata;
@@ -1708,11 +1643,6 @@ static int __init amd_pstate_init(void)
 	/* don't keep reloading if cpufreq_driver exists */
 	if (cpufreq_get_current_driver())
 		return -EEXIST;
-
-	quirks = NULL;
-
-	/* check if this machine need CPPC quirks */
-	dmi_check_system(amd_pstate_quirks_table);
 
 	switch (cppc_state) {
 	case AMD_PSTATE_UNDEFINED:
