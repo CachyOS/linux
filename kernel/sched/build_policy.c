88f919cdfcc068ec0faff2478b0d13d43a280ee8
Peter Jung <admin@ptr1337.dev>
2024-08-03 09:38:59 +0200

    Revert "sched_ext: Add sysrq-S which disables the BPF scheduler"
    This reverts commit 814ce9a87bf7f6c81f17c1fd7a4052d02a22087f.

--- kernel/sched/build_policy.c
+++ kernel/sched/build_policy.c
@@ -32,7 +32,6 @@
 #include <linux/suspend.h>
 #include <linux/tsacct_kern.h>
 #include <linux/vtime.h>
-#include <linux/sysrq.h>
 #include <linux/percpu-rwsem.h>
 
 #include <uapi/linux/sched/types.h>
