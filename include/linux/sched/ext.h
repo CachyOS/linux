ae29978fc8942f4ff68abc3651f2399a8a316b76
Peter Jung <admin@ptr1337.dev>
2024-08-03 09:38:56 +0200

    Revert "sched_ext: Implement DSQ iterator"
    This reverts commit 5ebb80db4ddd09bf33e53448ff31f8e25d1d0b75.

--- include/linux/sched/ext.h
+++ include/linux/sched/ext.h
@@ -61,7 +61,6 @@ struct scx_dispatch_q {
 	struct list_head	list;	/* tasks in dispatch order */
 	struct rb_root		priq;	/* used to order by p->scx.dsq_vtime */
 	u32			nr;
-	u32			seq;	/* used by BPF iter */
 	u64			id;
 	struct rhash_head	hash_node;
 	struct llist_node	free_node;
@@ -124,7 +123,6 @@ enum scx_kf_mask {
 
 struct scx_dsq_list_node {
 	struct list_head	node;
-	bool			is_bpf_iter_cursor;
 };
 
 /*
@@ -135,7 +133,6 @@ struct sched_ext_entity {
 	struct scx_dispatch_q	*dsq;
 	struct scx_dsq_list_node dsq_list;	/* dispatch order */
 	struct rb_node		dsq_priq;	/* p->scx.dsq_vtime order */
-	u32			dsq_seq;
 	u32			dsq_flags;	/* protected by DSQ lock */
 	u32			flags;		/* protected by rq lock */
 	u32			weight;
