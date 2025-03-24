d75f70dd0c9ab9f3e947301d21c29293f024a5ac
Luke Jones <luke@ljones.dev>
2025-03-24 15:41:05 +1300

    hid-asus-ally: do MCY FW validation in hid-asus-ally
    Export the validate_mcu_fw_version() symbol in namespace and use in
    hid-asus-ally if the driver is enabled.
    
    Signed-off-by: Luke Jones <luke@ljones.dev>

--- drivers/hid/hid-asus.c
+++ drivers/hid/hid-asus.c
@@ -24,6 +24,7 @@
  */
 
 #include <linux/acpi.h>
+#include "linux/export.h"
 #include <linux/dmi.h>
 #include <linux/hid.h>
 #include <linux/module.h>
@@ -694,7 +695,7 @@ static int mcu_request_version(struct hid_device *hdev)
 	return ret;
 }
 
-static void validate_mcu_fw_version(struct hid_device *hdev, int idProduct)
+void validate_mcu_fw_version(struct hid_device *hdev, int idProduct)
 {
 	int min_version, version;
 
@@ -722,12 +723,11 @@ static void validate_mcu_fw_version(struct hid_device *hdev, int idProduct)
 		set_ally_mcu_powersave(true);
 	}
 }
+EXPORT_SYMBOL_NS(validate_mcu_fw_version, "HID_ASUS");
 
 static int asus_kbd_register_leds(struct hid_device *hdev)
 {
 	struct asus_drvdata *drvdata = hid_get_drvdata(hdev);
-	struct usb_interface *intf;
-	struct usb_device *udev;
 	unsigned char kbd_func;
 	int ret;
 
@@ -755,12 +755,14 @@ static int asus_kbd_register_leds(struct hid_device *hdev)
 			return ret;
 	}
 
+	#if !IS_REACHABLE(CONFIG_HID_ASUS_ALLY)
 	if (drvdata->quirks & QUIRK_ROG_ALLY_XPAD) {
-		intf = to_usb_interface(hdev->dev.parent);
-		udev = interface_to_usbdev(intf);
+		struct usb_interface *intf = to_usb_interface(hdev->dev.parent);
+		struct usb_device *udev = interface_to_usbdev(intf);
 		validate_mcu_fw_version(hdev,
 			le16_to_cpu(udev->descriptor.idProduct));
 	}
+	#endif
 
 	drvdata->kbd_backlight = devm_kzalloc(&hdev->dev,
 					      sizeof(struct asus_kbd_leds),
