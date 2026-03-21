8644494e7a0f02663da983ddc965835dc770126e
Eric Naim <dnaim@cachyos.org>
2026-03-22 00:41:19 +0800

    Revert "applesmc: make applesmc_remove void"
    This reverts commit bb71014d2e5b0a8603dad52cd106ad3afd7d6899.
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- drivers/hwmon/applesmc.c
+++ drivers/hwmon/applesmc.c
@@ -979,7 +979,7 @@ out_mem:
 	return ret;
 }
 
-static void applesmc_remove(struct acpi_device *dev)
+static int applesmc_remove(struct acpi_device *dev)
 {
 	struct applesmc_device *smc = dev_get_drvdata(&dev->dev);
 
@@ -990,7 +990,7 @@ static void applesmc_remove(struct acpi_device *dev)
 	mutex_destroy(&smc->reg.mutex);
 	kfree(smc);
 
-	return;
+	return 0;
 }
 
 static acpi_status applesmc_walk_resources(struct acpi_resource *res,
