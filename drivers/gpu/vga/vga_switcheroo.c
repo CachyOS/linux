1859ccbf7cc4cd32bc59213361d6cc74c8aa2bd7
Aditya Garg <gargaditya08@live.com>
2026-03-28 17:12:19 +0000

    vga_switcheroo: Defer probe of display devices on T2 Macs
    apple-gmux: allow switching to igpu at probe is causing a regression
    on older non T2 Macs. Constrain the vga_switcheroo probe defer to T2
    Macs only.
    
    Signed-off-by: Aditya Garg <gargaditya08@live.com>

--- drivers/gpu/vga/vga_switcheroo.c
+++ drivers/gpu/vga/vga_switcheroo.c
@@ -31,6 +31,7 @@
 #define pr_fmt(fmt) "vga_switcheroo: " fmt
 
 #include <linux/apple-gmux.h>
+#include <linux/cachy-t2.h>
 #include <linux/console.h>
 #include <linux/debugfs.h>
 #include <linux/fb.h>
@@ -438,7 +439,8 @@ find_active_client(struct list_head *head)
 bool vga_switcheroo_client_probe_defer(struct pci_dev *pdev)
 {
 	if (pci_is_display(pdev)) {
-		if (apple_gmux_present() && !vgasr_priv.handler_flags)
+		if (apple_gmux_present() && !vgasr_priv.handler_flags &&
+		    (pdev != vga_default_device() || apple_is_t2_mac()))
 			return true;
 	}
 
