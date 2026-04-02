425f9a5246b41d4314c9478a2a552a09aea23084
Eric Naim <dnaim@cachyos.org>
2026-04-02 23:17:58 +0800

    Revert "vga_switcheroo: Defer probe of display devices on T2 Macs"
    This reverts commit 67316318f71c0252899e3342fcc50cc4f97959a2.
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- drivers/gpu/vga/vga_switcheroo.c
+++ drivers/gpu/vga/vga_switcheroo.c
@@ -31,7 +31,6 @@
 #define pr_fmt(fmt) "vga_switcheroo: " fmt
 
 #include <linux/apple-gmux.h>
-#include <linux/cachy-t2.h>
 #include <linux/console.h>
 #include <linux/debugfs.h>
 #include <linux/fb.h>
@@ -439,8 +438,7 @@ find_active_client(struct list_head *head)
 bool vga_switcheroo_client_probe_defer(struct pci_dev *pdev)
 {
 	if (pci_is_display(pdev)) {
-		if (apple_gmux_present() && !vgasr_priv.handler_flags &&
-		    (pdev != vga_default_device() || apple_is_t2_mac()))
+		if (apple_gmux_present() && !vgasr_priv.handler_flags)
 			return true;
 	}
 
