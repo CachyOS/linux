16b71275b1832466a86915ee68a417d25b8184c3
Namjae Jeon <linkinjeon@kernel.org>
2026-04-21 08:13:57 +0900

    ntfs: fix mmap_prepare writable check for shared mappings
    Linus pointed out that checking only VMA_WRITE_BIT is incorrect.
    Private writable mappings (MAP_PRIVATE) set VM_WRITE but do not
    write back to the filesystem. Also, mappings that can become
    writable via mprotect() (VM_MAYWRITE) must be handled.
    
    Use vma_desc_test_all(VMA_SHARED_BIT, VMA_MAYWRITE_BIT) instead,
    which matches what other filesystems do.
    
    [ naim: Use vma_desc_test_flags() twice because vma_desc_test_all is not
      available in 7.0 ]
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Reviewed-by: Hyunchul Lee <hyc.lee@gmail.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- fs/ntfs/file.c
+++ fs/ntfs/file.c
@@ -644,7 +644,8 @@ static int ntfs_file_mmap_prepare(struct vm_area_desc *desc)
 	if (NInoCompressed(NTFS_I(inode)))
 		return -EOPNOTSUPP;
 
-	if (vma_desc_test_flags(desc, VMA_WRITE_BIT)) {
+	if (vma_desc_test_flags(desc, VMA_SHARED_BIT) &&
+	    vma_desc_test_flags(desc, VMA_MAYWRITE_BIT)) {
 		struct inode *inode = file_inode(file);
 		loff_t from, to;
 		int err;
