9826cc2ae49ec96b572c79938ed94e58357368f9
Eric Naim <dnaim@cachyos.org>
2026-07-25 09:15:54 +0700

    fixup! ASoC: amd: yc: Add quirk for Acer Aspire A314-23P
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -580,7 +580,7 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 	{
 		.driver_data = &acp6x_card,
 		.matches = {
-			DMI_MATCH(DMI_BOARD_VENDOR, "RB"),
+			DMI_MATCH(DMI_BOARD_VENDOR, "MDU"),
 			DMI_MATCH(DMI_PRODUCT_NAME, "Aspire A314-23P"),
 		}
 	},
