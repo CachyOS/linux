evlaV/linux-integration

[FOR-UPSTREAM] ASoC: mute codec amp to protect against dsp fw failures
Add a new resume mute control to mute the speaker output on suspend
and only restore it once DSP firmware has successfully started
on resume.
This protects against short loud sounds upon resume if the DSP
firmware fails to start correctly, leavin the output muted.

Signed-off-by: Robert Beckett <bob.beckett@collabora.com>

sound/soc/codecs/max98388.c
@@ -298,6 +298,56 @@ static SOC_ENUM_SINGLE_DECL(max98388_ssm_mod_enum,
 			    MAX98388_SPK_AMP_SSM_MOD_SHIFT,
 			    max98388_ssm_mod_text);
 
+
+#define MAX98388_VOL_MUTE 0x7f
+
+static int max98388_resume_mute_switch_put(struct snd_kcontrol *kcontrol,
+					   struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *component = snd_soc_kcontrol_component(kcontrol);
+	struct max98388_priv *max98388 = snd_soc_component_get_drvdata(component);
+	int mute = ucontrol->value.integer.value[0];
+	unsigned int vol, val;
+	int ret;
+
+	ret = regmap_read(max98388->regmap, MAX98388_R2090_SPK_CH_VOL_CTRL, &vol);
+	if (ret)
+		return ret;
+
+	if (vol == MAX98388_VOL_MUTE && mute)
+		return 0;
+	if (vol != MAX98388_VOL_MUTE && !mute)
+		return 0;
+
+	if (mute) {
+		max98388->saved_vol = vol;
+		val = MAX98388_VOL_MUTE;
+	} else {
+		val = max98388->saved_vol;
+	}
+
+	ret = regmap_write(max98388->regmap, MAX98388_R2090_SPK_CH_VOL_CTRL, val);
+
+	return ret;
+}
+
+static int max98388_resume_mute_switch_get(struct snd_kcontrol *kcontrol,
+					   struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *component = snd_soc_kcontrol_component(kcontrol);
+	struct max98388_priv *max98388 = snd_soc_component_get_drvdata(component);
+	unsigned int vol;
+	int ret;
+
+	ret = regmap_read(max98388->regmap, MAX98388_R2090_SPK_CH_VOL_CTRL, &vol);
+	if (ret)
+		return ret;
+
+	ucontrol->value.integer.value[0] = (vol == MAX98388_VOL_MUTE) ? 1 : 0;
+
+	return 0;
+}
+
 static const struct snd_kcontrol_new max98388_snd_controls[] = {
 	SOC_SINGLE("Ramp Up Switch", MAX98388_R2091_SPK_CH_CFG,
 		   MAX98388_SPK_CFG_VOL_RMPUP_SHIFT, 1, 0),
@@ -338,6 +388,8 @@ static const struct snd_kcontrol_new max98388_snd_controls[] = {
 	/* Digital Volume */
 	SOC_SINGLE_TLV("Digital Volume", MAX98388_R2090_SPK_CH_VOL_CTRL,
 		       0, 0x7F, 1, max98388_digital_tlv),
+	SOC_SINGLE_EXT("Resume Mute Switch", 0, 0, 1, 0,
+		       max98388_resume_mute_switch_get, max98388_resume_mute_switch_put),
 	/* Speaker Volume */
 	SOC_SINGLE_TLV("Speaker Volume", MAX98388_R2092_SPK_AMP_OUT_CFG,
 		       0, 5, 0, max98388_amp_gain_tlv),

CachyOS/linux

Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

sound/soc/codecs/max98388.c
@@ -297,6 +297,56 @@ static SOC_ENUM_SINGLE_DECL(max98388_ssm_mod_enum,
 			    MAX98388_SPK_AMP_SSM_MOD_SHIFT,
 			    max98388_ssm_mod_text);
 
+
+#define MAX98388_VOL_MUTE 0x7f
+
+static int max98388_resume_mute_switch_put(struct snd_kcontrol *kcontrol,
+					   struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *component = snd_kcontrol_chip(kcontrol);
+	struct max98388_priv *max98388 = snd_soc_component_get_drvdata(component);
+	int mute = ucontrol->value.integer.value[0];
+	unsigned int vol, val;
+	int ret;
+
+	ret = regmap_read(max98388->regmap, MAX98388_R2090_SPK_CH_VOL_CTRL, &vol);
+	if (ret)
+		return ret;
+
+	if (vol == MAX98388_VOL_MUTE && mute)
+		return 0;
+	if (vol != MAX98388_VOL_MUTE && !mute)
+		return 0;
+
+	if (mute) {
+		max98388->saved_vol = vol;
+		val = MAX98388_VOL_MUTE;
+	} else {
+		val = max98388->saved_vol;
+	}
+
+	ret = regmap_write(max98388->regmap, MAX98388_R2090_SPK_CH_VOL_CTRL, val);
+
+	return ret;
+}
+
+static int max98388_resume_mute_switch_get(struct snd_kcontrol *kcontrol,
+					   struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *component = snd_kcontrol_chip(kcontrol);
+	struct max98388_priv *max98388 = snd_soc_component_get_drvdata(component);
+	unsigned int vol;
+	int ret;
+
+	ret = regmap_read(max98388->regmap, MAX98388_R2090_SPK_CH_VOL_CTRL, &vol);
+	if (ret)
+		return ret;
+
+	ucontrol->value.integer.value[0] = (vol == MAX98388_VOL_MUTE) ? 1 : 0;
+
+	return 0;
+}
+
 static const struct snd_kcontrol_new max98388_snd_controls[] = {
 	SOC_SINGLE("Ramp Up Switch", MAX98388_R2091_SPK_CH_CFG,
 		   MAX98388_SPK_CFG_VOL_RMPUP_SHIFT, 1, 0),
@@ -337,6 +387,8 @@ static const struct snd_kcontrol_new max98388_snd_controls[] = {
 	/* Digital Volume */
 	SOC_SINGLE_TLV("Digital Volume", MAX98388_R2090_SPK_CH_VOL_CTRL,
 		       0, 0x7F, 1, max98388_digital_tlv),
+	SOC_SINGLE_EXT("Resume Mute Switch", 0, 0, 1, 0,
+		       max98388_resume_mute_switch_get, max98388_resume_mute_switch_put),
 	/* Speaker Volume */
 	SOC_SINGLE_TLV("Speaker Volume", MAX98388_R2092_SPK_AMP_OUT_CFG,
 		       0, 5, 0, max98388_amp_gain_tlv),